7938c916521b08e9a5ecf38c8845192a0a1cad68
omaranBazna <omran4d@gmail.com>
2022-07-16 00:35:43 -0400

    finishing CandyHeartCard

--- src/components/CandyCardHeart.js
+++ src/components/CandyCardHeart.js
@@ -8,9 +8,39 @@ const colors = [
     '#e3b8f5',
     '#fffcf2'
 ];
-const CandyCardHeart=()=>{
-    return (
-        <HeartSvg color="red" />
+const CandyCardHeart=(props)=>{
+
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
+    const random=shuffle(colors)[0]
+    return ( 
+        <div className="heart">
+            <div className="heart-img">
+                <HeartSvg color={random} />
+            </div>
+            <p className="heart-message">
+                  {props.message}
+            </p>
+           
+
+        </div>
+
+        
     )
 }
 export default CandyCardHeart;
