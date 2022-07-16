b4e7529746cc4dcc875f4ada3ae6dcc57737ca70
omaranBazna <omran4d@gmail.com>
2022-07-16 00:30:42 -0400

    add the color array

--- src/components/CandyCardHeart.js
+++ src/components/CandyCardHeart.js
@@ -1,4 +1,13 @@
 import HeartSvg from './HeartSvg'
+
+const colors = [
+    '#b8e3f5',
+    '#f5b8e4',
+    '#b8f5b9',
+    '#f5f3b8',
+    '#e3b8f5',
+    '#fffcf2'
+];
 const CandyCardHeart=()=>{
     return (
         <HeartSvg color="red" />
