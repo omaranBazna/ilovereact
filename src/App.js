7cf3d9d044a56d311ab6b9b4e7685b526b013aec
omaranBazna <omran4d@gmail.com>
2022-07-15 23:29:03 -0400

    define the Heart stateless funtion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 const message="cool cud"
+
+
+const Heart=(props)=>{
+  return (
+    <div>
+  
+    </div>
+  )
+}
 function App() {
   return (
     <Heart msg ={message} />
