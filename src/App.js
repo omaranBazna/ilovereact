16c0e3cbe0bd790c08543fdbf0b21c68a1eaa970
omaranBazna <omran4d@gmail.com>
2022-07-15 23:55:44 -0400

    add the Heart log

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import HeartLogo from './assests/heart.svg';
+
 import './App.css';
 import Header from './components/Header'
 import Heart from "./components/Heart"
--- src/components/Heart.js
+++ src/components/Heart.js
@@ -1,4 +1,4 @@
-
+import HeartLogo from '../assests/heart.svg';
 const Heart=(props)=>{
     return (
       <div>
