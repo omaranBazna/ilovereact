9b0143d3a1a8a80f0cbece993ee37d298bf7cc37
omaranBazna <omran4d@gmail.com>
2022-07-15 23:34:03 -0400

    correct the image src

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const message="cool cud"
 const Heart=(props)=>{
   return (
     <div>
-      <img className="heart-img" src={logo.svg} alt="heart"></img>
+      <img className="heart-img" src={logo} alt="heart"></img>
       <p className="heart-message">{props.msg}</p>
     </div>
   )
